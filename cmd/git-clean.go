944363d407d1c2157e0273e0bc1a7fddc3500f49
Yohannes Alemu <johannesalemu01@gmail.com>
2026-03-11 15:44:19 +0300

    Enhance git-clean command to handle no merged branches and confirm deletion

--- cmd/git-clean.go
+++ cmd/git-clean.go
@@ -33,48 +33,118 @@ Usage Examples:
   devtool git-clean --dry-run
 `,
 	Run: func(cmd *cobra.Command, args []string) {
-		output, err:= exec.Command("git", "branch", "--merged").Output()
-		if err != nil{
+		output, err := exec.Command("git", "branch", "--merged").Output()
+		if err != nil {
 			fmt.Printf("Error: Are you inside a git repository?")
 			return
 		}
- lines:=strings.Split(string(output),"\n") 
- var branches []string
+		lines := strings.Split(string(output), "\n")
+		var branches []string
 
- for _, line := range lines{
-	branch := strings.TrimSpace(line)
-	if branch == "" || strings.Contains(branch,"main") || strings.Contains(branch,"*"){
-		continue
-	}
-	branches = append(branches, branch)
- }
+		for _, line := range lines {
+			branch := strings.TrimSpace(line)
+			if branch == "" || strings.Contains(branch, "main") || strings.Contains(branch, "*") {
+				continue
+			}
+			branches = append(branches, branch)
+		}
 
- if len(branches) == 0{
-	fmt.Println(" No merged branches found to delete.")
-	return
- }
+		if len(branches) == 0 {
+			fmt.Println(" No merged branches found to delete.")
+			return
+		}
 		fmt.Println("Merged branches:")
-		for _, branch := range branches{
-			fmt.Println("-",branch)
+		for _, branch := range branches {
+			fmt.Println("-", branch)
 		}
 
 		var confirm string
 		fmt.Print("Do you want to delete these branches? (y/N): ")
 		fmt.Scanln(&confirm)
 
+		if strings.ToLower(confirm) != "y" {
+			fmt.Println("Cancelled. No branches were deleted.")
+			return
+		}
+
+		for _, branch := range branches {
+			fmt.Println("Deleting branch:", branch)
+			cmd := exec.Command("git", "branch", "-d", branch)
+			err := cmd.Run()
+			if err != nil {
+				fmt.Printf("Error deleting branch %s: %v\n", branch, err)
+			}
+		}
+		fmt.Println("Cleanup complete!")
+
+	},
+}
+
+var gitListCmd = &cobra.Command{
+	Use:   "git-list",
+	Short: "List merged and Unmerged branches",
+	Long: `Git List shows all local git branches and separates
+them into merged and unmerged branches for easier cleanup.
+
+Usage Examples:
+
+  # List branches
+  devtool git-list
+`,
+
+	Run: func(cmd *cobra.Command, args []string) {
+		mergedOut, err := exec.Command("git", "branch", "--merged").Output()
+		if err != nil {
+			fmt.Println("Error: Are you inside a git repository?")
+			return
+		}
+
+		// Get unmerged branches
+		unMergedOut, err := exec.Command("git", "branch", "--no-merged").Output()
+		if err != nil {
+			fmt.Println("Error: Are you inside a git repository?")
+			return
+		}
+		// Helper function to parse branches
+		parseBranches := func(output []byte) []string {
+			var result []string
+			lines := strings.Split(string(output), "\n")
+			for _, line := range lines {
+				branch := strings.TrimSpace(line)
+				if branch == "" || strings.Contains(branch, "*") { //git markes the current branch with a * so we skip it
+					continue
+				}
+				result = append(result, branch)
+			}
+			return result
+		}
+
+		merged := parseBranches(mergedOut)
+		unMerged := parseBranches(unMergedOut)
+
+		//display branches
+
+		fmt.Println("Merged branches:")
+		if len(merged) == 0 {
+			fmt.Println("None")
+		} else {
+
+			for _, branch := range merged {
+				fmt.Println("-", branch)
+			}
+		}
+		fmt.Println("Unmerged branches:")
+		if len(unMerged) == 0 {
+			fmt.Println("None")
+		} else {
+			for _, branch := range unMerged {
+				fmt.Println("-", branch)
+			}
+		}
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(gitCleanCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// gitCleanCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// gitCleanCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	rootCmd.AddCommand(gitListCmd)
 }
